2b05939aa097e3bdc73a6498637a3f6a18fc60df
Adl Gamaladdin <gamaladdinadl@gmail.com>
2025-04-30 23:43:08 -0700

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>&  shifts,
+    size_t day,
+    size_t curr
+);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +45,44 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t n = avail.size(); // number of days
+    size_t k = avail[0].size(); // number of workers
+    sched.resize(n);
+    std::vector<size_t> shifts(k, 0); //stores how many shifts an employee has worked in n-day period
+    return schedHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
+}
 
+bool schedHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>&  shifts,
+    size_t day,
+    size_t curr
+){
+    size_t n = avail.size(); // number of days
+    size_t k = avail[0].size(); // number of workers
+    if(day == n){ // if we've scheduled all days then we're done
+        return true;
+    }
+ 
+    if(curr == dailyNeed){ // if the day already has enough workers we go on to the next
+        return schedHelper(avail, dailyNeed, maxShifts, sched, shifts, day+1, 0);
+    }
 
+    for(size_t i = 0; i < k; i++){ //for all the workers on a given day
+        if(avail[day][i] && shifts[i] < maxShifts && std::find(sched[day].begin(), sched[day].end(), i) == sched[day].end()){ //if they are avalible and haven't worked the max shifts for the week and have not already been scheduled fo rthat day
+            sched[day].push_back(i); //we schedule them for the day
+            shifts[i]++;// increase their number of shifts
 
+            if(schedHelper(avail, dailyNeed, maxShifts, sched, shifts, day, curr+1)){// increase the count of workers for the day if it returns true that means we are done
+                return true;
+            }
 
+            sched[day].pop_back(); //else we remove the day from to be scheduled 
+            shifts[i]--;
+        }
+    }
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,10 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordlehelper( const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict, const std::map<int, std::set<char>>& possible, 
+    std::string& current, int index, std::set<std::string>& result);
 
 
 // Definition of primary wordle function
@@ -22,7 +26,77 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int size = in.size();
+    std::map<int, std::set<char>> possible; //will store all possible letters
+
+    for(std::set<std::string>::const_iterator it = dict.begin(); it != dict.end(); it++){ //loops through all possible words
+        const std::string& temp = *it;
+        if(temp.size() != size){ //if the word is the wrong size then we skip it
+        }
+        else{
+            bool fixed = true;
+            for(int i = 0; i < size; i++){ // this checks to see if all the fixed letters from in are in our dict word where theyre supposed to be
+                if(in[i] != '-' && temp[i] != in[i]){
+                    fixed = false;
+                    break;
+                }
+            }
+            if (fixed){
+                for(int i = 0; i < size; i++){
+                    if(in[i] == '-'){
+                        char c = temp[i];
+                        if(std::islower(c)){
+                           possible[i].insert(c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    std::set<std::string> result;
+    std::string current = in;
+    wordlehelper(in, floating, dict, possible, current, 0, result);
+    return result;
 
 }
 
 // Define any helper functions here
+void wordlehelper( const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    const std::map<int, std::set<char>>& possible,
+    std::string& current, 
+    int index, 
+    std::set<std::string>& result)
+    {
+        int size = in.size();
+        if (index == size){ //base case
+            std::string temp = current;
+            for(int i = 0; i < floating.size(); i++){
+                char tmp = floating[i];
+                if(temp.find(tmp) == std::string::npos){ //situation where full test word doesn't include floating letters
+                    return;
+                }
+                temp[temp.find(tmp)] = '-'; //gets rid of duplicates after we check
+            }
+            
+            if(dict.find(current) != dict.end()){ // check to see if word is in dictionary
+                result.insert(current);
+            }
+            return;
+        }
+
+        if(in[index] != '-'){ //if there is already a fixed letter in this slot
+            wordlehelper(in, floating, dict, possible, current, index + 1, result);
+        }
+
+        else{
+            const std::set<char>& choice = possible.at(index);
+            for(std::set<char>::const_iterator it = choice.begin(); it != choice.end(); it++){ //tests all combinations
+                current[index] = *it;
+                wordlehelper(in, floating, dict, possible, current, index + 1, result);
+            }
+            current[index] = '-';
+        }
+
+    }
